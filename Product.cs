16c8d15791d8980028ae09203b1f8d2baed0c8ec
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-09-19 16:27:13 +0200

    refactor following tips

--- Product.cs
+++ Product.cs
@@ -2,6 +2,101 @@ namespace csharp_oop_shop;
 
 public class Product
 {
+    private int _code;
+    private string _name;
+    private string _description;
+    private decimal _price;
+    private float _vat;
+
+    public Product(string name, decimal price, float vat)
+    {
+        // _code = new Random().Next(1, 99999999);
+        _code = 12;
+        _name = name;
+        _price = price;
+        _vat = vat;
+        _description = String.Empty;
+    }
+    
+    public Product(string name, string description, decimal price, float vat)
+        : this(name, price, vat)
+    {
+        _description = description;
+    }
+
+    public decimal GetTaxedPrice()
+    {
+        return _price + (_price * (decimal) _vat);
+    }
+
+    public string GetFormattedCode()
+    {
+        string codeAsString = _code.ToString();
+        string formattedCode = String.Empty;
+        int padding = 8;
+
+        while (formattedCode.Length < padding - codeAsString.Length)
+        {
+            formattedCode += '0';
+        }
+
+        formattedCode += codeAsString;
+
+        return formattedCode;
+    }
+
+    public string GetFullName()
+    {
+        string paddedCode = GetFormattedCode();
+        return $"{paddedCode} - {_name}";
+    }
+
+    public int GetCode()
+    {
+        return _code;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public void SetName(string name)
+    {
+        _name = name;
+    }
+
+    public string GetDescription()
+    {
+        return _description;
+    }
+
+    public void SetDescription(string description)
+    {
+        _description = description;
+    }
+
+    public decimal GetPrice()
+    {
+        return _price;
+    }
+
+    public void SetPrice(decimal price)
+    {
+        _price = price;
+    }
+
+    public float GetVat()
+    {
+        return _vat;
+    }
+
+    public void SetVat(float vat)
+    {
+        _vat = vat;
+    }
+
+    /*
     public int Code { get; }
     public string Name { get; set; }
     public string Description { get; set; }
@@ -26,4 +121,5 @@ public class Product
     {
         Description = description;
     }
+    */
 }
--- Program.cs
+++ Program.cs
@@ -2,13 +2,13 @@
 
 Product product = new Product("Mouse", 80m, 0.22f);
 
-Console.WriteLine(product.FullName);
-Console.WriteLine(product.TaxedPrice);
+Console.WriteLine(product.GetFullName());
+Console.WriteLine(product.GetTaxedPrice());
 
-product.Name = "Keyboard";
-product.Description = "Cool mechanical keyboard";
-product.Price = 142.99m;
-product.Vat = 0.18f;
+product.SetName("Keyboard");
+product.SetDescription("Cool mechanical keyboard");
+product.SetPrice(142.99m);
+product.SetVat(0.18f);
 
-Console.WriteLine(product.FullName);
-Console.WriteLine(product.TaxedPrice);
+Console.WriteLine(product.GetFullName());
+Console.WriteLine(product.GetTaxedPrice());
